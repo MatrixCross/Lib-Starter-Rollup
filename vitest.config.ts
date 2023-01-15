f52566cb12e8e705004cc4bc22c5e8ad06bd3e60
樱吹雪 <wyatex@qq.com>
2023-01-15 13:56:21 +0800

    build: 完善项目配置

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   test: {
     coverage: {
-      reporter: ['text', 'json', 'html'],
+      reporter: ['text', 'json', 'html', 'json-summary'],
     },
   },
 })
